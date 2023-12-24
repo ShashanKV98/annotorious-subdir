50d78607ba404f138d9c47e8d3ac98a14725757f
ShashanKV98 <vshashank32@gmail.com>
2023-12-24 15:05:15 -0700

    Update Freehand.ts

--- src/model/core/freehand/Freehand.ts
+++ src/model/core/freehand/Freehand.ts
@@ -1,1 +1,15 @@
+import type { Bounds, Geometry, Shape } from '../Shape';
 
+export interface Freehand extends Shape {
+
+  geometry: FreehandGeometry;
+
+}
+
+export interface FreehandGeometry extends Geometry {
+
+  points: Array<Array<number>>;
+
+  bounds: Bounds;
+  
+}
