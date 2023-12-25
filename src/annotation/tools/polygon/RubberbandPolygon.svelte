582ead5db1360ef685003afb2fcc0d2c00c39545
ShashanKV98 <vshashank32@gmail.com>
2023-12-25 00:06:04 -0700

    Update RubberbandPolygon.svelte

--- src/annotation/tools/polygon/RubberbandPolygon.svelte
+++ src/annotation/tools/polygon/RubberbandPolygon.svelte
@@ -1,4 +1,4 @@
-<script type="ts">
+<script type="ts" lang="ts">
   import { onMount, createEventDispatcher } from 'svelte';
   import type { DrawingMode } from '../../../AnnotoriousOpts';
   import { boundsFromPoints, computeArea, ShapeType, type Polygon } from '../../../model';
