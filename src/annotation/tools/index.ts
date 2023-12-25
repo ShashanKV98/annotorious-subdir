082240328542a0ea5c8d77fffc0a0231dd9f14a0
ShashanKV98 <vshashank32@gmail.com>
2023-12-25 00:36:40 -0700

    Update index.ts

--- src/annotation/tools/index.ts
+++ src/annotation/tools/index.ts
@@ -1,4 +1,5 @@
 export { default as ToolMount } from './ToolMount.svelte';
 export * from './rectangle';
+export * from './ellipse';
 export * from './DrawingToolConfig';
 export * from './drawingToolsRegistry';
