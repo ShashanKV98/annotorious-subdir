001139cc889d0a87c78033428b61949e2f948a74
ShashanKV98 <vshashank32@gmail.com>
2023-12-24 22:51:21 -0700

    Update Freehand.svelte

--- src/annotation/shapes/Freehand.svelte
+++ src/annotation/shapes/Freehand.svelte
@@ -1,1 +1,26 @@
+<script type="ts">
+  import type { DrawingStyle } from '@annotorious/core';
+  import type { Geometry, ImageAnnotation, FreehandGeometry } from '../../model';
+  import { computeStyle } from '../utils/styling';
+  
+  /** Props **/
+  export let annotation: ImageAnnotation;
+  export let geom: Geometry;
+  export let style: DrawingStyle | ((annotation: ImageAnnotation) => DrawingStyle) = undefined;
 
+  $: computedStyle = computeStyle(annotation, style);
+
+  const { d } = geom as FreehandGeometry;
+</script>
+
+<g data-id={annotation.id}>
+  <path 
+    class="a9s-outer"
+    style={computedStyle ? 'display:none;' : undefined}
+    points={points.map(xy => xy.join(',')).join(' ')} />
+
+  <path 
+    class="a9s-inner"
+    style={computedStyle}
+    points={points.map(xy => xy.join(',')).join(' ')} />
+</g>
