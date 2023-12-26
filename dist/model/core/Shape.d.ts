97448d802f9827dc04890ee628ce040b97d8eef3
ShashanKV98 <u1265559@utah.edu>
2023-12-26 16:18:47 -0700

    Added dist

---
+++ dist/model/core/Shape.d.ts
@@ -0,0 +1,21 @@
+import type { AbstractSelector } from '@annotorious/core';
+export interface Shape extends AbstractSelector {
+    type: ShapeType;
+    geometry: Geometry;
+}
+export declare enum ShapeType {
+    ELLIPSE = "ELLIPSE",
+    POLYGON = "POLYGON",
+    RECTANGLE = "RECTANGLE",
+    FREEHAND = "FREEHAND"
+}
+export interface Geometry {
+    bounds: Bounds;
+}
+export interface Bounds {
+    minX: number;
+    minY: number;
+    maxX: number;
+    maxY: number;
+}
+//# sourceMappingURL=Shape.d.ts.map
