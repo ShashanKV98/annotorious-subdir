a047aa35b3bdff30bd821bc862fc46c4ecac87ca
ShashanKV98 <vshashank32@gmail.com>
2023-12-25 19:38:12 -0700

    Update editorsRegistry.ts

--- src/annotation/editors/editorsRegistry.ts
+++ src/annotation/editors/editorsRegistry.ts
@@ -2,12 +2,10 @@ import { ShapeType, type Shape } from '../../model';
 import type { SvelteComponent } from 'svelte';
 import { PolygonEditor } from './polygon';
 import { RectangleEditor } from './rectangle';
-import { EllipseEditor } from './ellipse';
 
 const REGISTERED = new Map<ShapeType, typeof SvelteComponent>([
   [ShapeType.RECTANGLE, RectangleEditor],
   [ShapeType.POLYGON, PolygonEditor],
-  [ShapeType.ELLIPSE,EllipseEditor],
 ]);
 
 export const getEditor = (shape: Shape) => REGISTERED.get(shape.type);
