1da63186e8ac579f1c271001215246ca09ec1a14
ShashanKV98 <vshashank32@gmail.com>
2023-12-26 16:17:57 -0700

    Update index.ts

--- src/annotation/shapes/index.ts
+++ src/annotation/shapes/index.ts
@@ -1,4 +1,3 @@
 export { default as Ellipse } from './Ellipse.svelte';
 export { default as Polygon } from './Polygon.svelte';
 export { default as Rectangle } from './Rectangle.svelte';
-export { default as Freehand } from './Freehand.svelte';
