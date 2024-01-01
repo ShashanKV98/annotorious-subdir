2b4aa1a8936969d967f35e9aedaf0e8cb6eb645c
ShashanKV98 <u1265559@utah.edu>
2024-01-01 15:52:39 -0700

    modified

--- src/annotation/utils/path.ts
+++ src/annotation/utils/path.ts
@@ -23,57 +23,74 @@ export const options = {
 
 const average = (a, b) => (a + b) / 2
 
-// export function getSvgPathFromStroke(points, closed = true) {
-//   const len = points.length
+function getControlPoints(x0, y0, x1, y1, x2, y2, t) {
+  // t is the tension and typically between 0 (loose) and 1 (tight)
+  const d01 = Math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
+  const d12 = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
-//   if (len < 4) {
-//     return ``
-//   }
+  const fa = (t * d01) / (d01 + d12) // scaling factor for control point 1
+  const fb = (t * d12) / (d01 + d12) // scaling factor for control point 2
 
-//   let a = points[0]
-//   let b = points[1]
-//   const c = points[2]
+  const p1x = x1 + fa * (x0 - x2) // x2 control point for x1
+  const p1y = y1 + fa * (y0 - y2) // y2 control point for y1
 
-//   let result = `M${a[0].toFixed(2)},${a[1].toFixed(2)} Q${b[0].toFixed(
-//     2
-//   )},${b[1].toFixed(2)} ${average(b[0], c[0]).toFixed(2)},${average(
-//     b[1],
-//     c[1]
-//   ).toFixed(2)} T`
+  const p2x = x1 - fb * (x0 - x2) // x2 control point for x2
+  const p2y = y1 - fb * (y0 - y2) // y2 control point for y2
 
-//   for (let i = 2, max = len - 1; i < max; i++) {
-//     a = points[i]
-//     b = points[i + 1]
-//     result += `${average(a[0], b[0]).toFixed(2)},${average(a[1], b[1]).toFixed(
-//       2
-//     )} `
-//   }
-
-//   if (closed) {
-//     result += 'Z'
-//   }
-
-//   return result
-// }
-
-
-export function getSvgPathFromStroke(stroke) {
-  // let stroke = simplify(strokePrev);
-  // console.log("Stroke", stroke);
-  if (!stroke.length) return ''
-
-  const d = stroke.reduce(
-    (acc, [x0, y0], i, arr) => {
-      const [x1, y1] = arr[(i + 1) % arr.length]
-      acc.push(x0, y0, (x0 + x1) / 2, (y0 + y1) / 2)
-      return acc
-    },
-    ['M', ...stroke[0], 'Q']
-  )
-
-  d.push('Z')
-  return d.join(' ')
+  return [p1x, p1y, p2x, p2y]
 }
+
+export function getSvgPathFromStroke(points, closed = true) {
+  const len = points.length
+
+  if (len < 4) {
+    return ``
+  }
+
+  let a = points[0]
+  let b = points[1]
+  const c = points[2]
+
+  let result = `M${a[0].toFixed(2)},${a[1].toFixed(2)} Q${b[0].toFixed(
+    2
+  )},${b[1].toFixed(2)} ${average(b[0], c[0]).toFixed(2)},${average(
+    b[1],
+    c[1]
+  ).toFixed(2)} T`
+
+  for (let i = 2, max = len - 1; i < max; i++) {
+    a = points[i]
+    b = points[i + 1]
+    result += `${average(a[0], b[0]).toFixed(2)},${average(a[1], b[1]).toFixed(
+      2
+    )} `
+  }
+
+  if (closed) {
+    result += 'Z'
+  }
+
+  return result
+}
+
+
+// export function getSvgPathFromStroke(stroke) {
+//   // let stroke = simplify(strokePrev);
+//   // console.log("Stroke", stroke);
+//   if (!stroke.length) return ''
+
+//   const d = stroke.reduce(
+//     (acc, [x0, y0], i, arr) => {
+//       const [x1, y1] = arr[(i + 1) % arr.length]
+//       acc.push(x0, y0, (x0 + x1) / 2, (y0 + y1) / 2)
+//       return acc
+//     },
+//     ['M', ...stroke[0], 'C']
+//   )
+
+//   d.push('Z')
+//   return d.join(' ')
+// }
 
 export function getSmoothPathData(
   points: Array<Array<number>>,
